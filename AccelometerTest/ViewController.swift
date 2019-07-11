f538837966099f69694b9d900eca4371220e408a
Nova Arisma <novaarisma95@gmail.com>
2019-07-11 15:45:57 +0700

    Initial Commit

---
+++ AccelometerTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AccelometerTest
+//
+//  Created by Nova Arisma on 11/07/19.
+//  Copyright © 2019 Nova Arisma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
